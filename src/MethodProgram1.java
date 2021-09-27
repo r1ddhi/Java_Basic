b730e2495516725108e8926f7079dad63dd10fb4
riddhi kumbhani <riddhikumbhani0402@gmail.com>
2021-09-27 10:53:43 +0530

    Syntax of the method program

---
+++ src/MethodProgram1.java
@@ -0,0 +1,9 @@
+public class MethodProgram1 {
+    static void telljoke(){
+        System.out.println("i invented something ");
+    }
+
+    public static void main(String[] args) {
+        telljoke();
+    }
+}
