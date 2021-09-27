115e1860f63cb3c5523476b834c002bc6a25da45
riddhi kumbhani <riddhikumbhani0402@gmail.com>
2021-09-27 11:37:24 +0530

    Setting a table using array in which you can set array length and enter as manny integers you want the table for, and it will show you the table for the same (just tried this so that I don't have to set loops again and again or rerun it n number of times)

--- src/table_of_a_given_num.java
+++ src/table_of_a_given_num.java
@@ -2,20 +2,25 @@ import java.util.Scanner;
 
 public class table_of_a_given_num {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("ENTER THE NUMBER : ");
-        int num = input.nextInt();
-        for (int k = 0; k < 10; k++)
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of integers for which the table has to be searched : ");//checks the length of array
+        int l= sc.nextInt();//stores the length
+        int[] num;//initialising the array
+        num=new int[l];
+        System.out.println("Enter the numbers for which the table has to be found: ");//takes the input of number for which the table is to be found
+        for (int i = 0 ;i< num.length;i++)//normal for loop to store the array input
         {
-            System.out.println(num + " x " + (k+1) + " = " +
-                    (num * (k+1)));
+         num[i] = sc.nextInt();
         }
-        System.out.println("ENTER THE OTHER NUMBER : ");
-        int newnu = input.nextInt();
-        for (int i = 0; i < 10; i++)
-        {
-            System.out.println( newnu+ " x " + (i+1) + " = " +
-                    (num * (i+1)));
+        System.out.println("The tables are : ");//for the tables
+        for (int j = 0;j< num.length;j++)//setting the array measures
+             {
+            for (int i = 1; i <= 10; i++) //setting the table
+                 {
+                System.out.println(num[j] + " x " + i + " = " + num[j] * i);//format to show the table
+                System.out.println("");//for better appearance
+            }
+            System.out.println("\n");//for better appearance
         }
     }
 }
